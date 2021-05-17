a067838d4afd6a4c6f3adb4db15b37f76296de71
Stephen Nneji <steve.nneji@gmail.com>
2021-05-17 17:00:19 +0100

    Adds unit tests

---
+++ test.py
@@ -0,0 +1,139 @@
+import os
+import shutil
+import tempfile
+import unittest
+import h5py
+import numpy as np
+import tifffile
+import writer
+
+
+class TestWriter(unittest.TestCase):
+    def setUp(self):
+        # Create a temporary directory
+        self.test_dir = tempfile.mkdtemp()
+        self.file_descriptors = []
+
+    def tearDown(self):
+        # Remove the directory after the test
+        for fd in self.file_descriptors:
+            os.close(fd)
+        shutil.rmtree(self.test_dir)
+
+    def testAddNxtomoEntry(self):
+        filename = os.path.join(self.test_dir, 'output.nxs')
+        image_names = []
+        image_keys = [2, 2, 2, 0, 0, 0, 0, 1, 1, 1]
+        angles = [0, 0, 0, 0, 45, 90, 180, 180, 180, 180]
+
+        rng = np.random.default_rng()
+        rints = rng.integers(0, np.iinfo(np.uint16).max, size=(10, 128, 128), dtype=np.uint16)
+        for i in range(rints.shape[0]):
+            image_names.append(os.path.join(self.test_dir, f'image_{i}.tif'))
+            tifffile.imsave(image_names[-1], rints[i])
+
+        writer.add_nxtomo_entry(filename, image_names, image_keys, angles)
+
+        with h5py.File(filename, 'r') as new_file:
+            np.testing.assert_array_equal(rints, new_file['/entry/tomo_entry/data/data'])
+            np.testing.assert_array_equal(image_keys, new_file['/entry/tomo_entry/data/image_key'])
+            np.testing.assert_array_almost_equal(angles, new_file['/entry/tomo_entry/data/rotation_angle'])
+            self.assertEqual(new_file['/entry/tomo_entry/sample/name'][()], b'')
+            self.assertEqual(new_file['/entry/tomo_entry/definition'][()], b'NXtomo')
+            self.assertEqual(new_file['/entry/tomo_entry/title'][()], b'')
+
+        filename = os.path.join(self.test_dir, 'output.nxs')
+       
+        with h5py.File(filename, 'w') as new_file:
+            main_entry = new_file.create_group('entry')
+            main_entry.attrs['NX_class'] = 'NXentry'
+            main_entry['title'] = 'Title'
+            main_entry['sample/name'] = 'Sample'
+        
+        writer.add_nxtomo_entry(filename, image_names, image_keys, angles)
+
+        with h5py.File(filename, 'r') as new_file:
+            np.testing.assert_array_equal(rints, new_file['/entry/tomo_entry/data/data'])
+            np.testing.assert_array_equal(image_keys, new_file['/entry/tomo_entry/data/image_key'])
+            np.testing.assert_array_almost_equal(angles, new_file['/entry/tomo_entry/data/rotation_angle'])
+            self.assertEqual(new_file['/entry/tomo_entry/sample/name'][()], b'Sample')
+            self.assertEqual(new_file['/entry/tomo_entry/definition'][()], b'NXtomo')
+            self.assertEqual(new_file['/entry/tomo_entry/title'][()], b'Title')
+   
+    @staticmethod
+    def createFakeImages(dir, count=1):
+        paths = []
+        
+        for i in range(count):
+            ext = '.TIF' if i % 2 else '.tiff'
+            try:
+                fd, tp =tempfile.mkstemp(dir=dir, prefix=f'{i}', suffix=ext)
+                paths.append(tp)
+            finally:
+                os.close(fd)
+
+        return paths
+
+    def testPrepareImages(self):
+        self.assertRaises(ValueError, writer.prepare_images, [], '')
+
+        proj_dir = tempfile.mkdtemp(dir=self.test_dir)
+        
+        proj_paths = self.createFakeImages(proj_dir, 2)
+        names, keys, angles = writer.prepare_images([0, 170], proj_dir)
+        self.assertEqual(names, proj_paths)
+        self.assertEqual(keys, [0, 0])
+        self.assertEqual(angles, [0, 170])
+
+        self.assertRaises(ValueError, writer.prepare_images, [0, 85, 170], proj_dir)
+
+        dark_before_dir = tempfile.mkdtemp(dir=self.test_dir)
+        self.assertRaises(FileNotFoundError, writer.prepare_images, [0, 170], proj_dir, dark_before_dir)
+        
+        db_paths = self.createFakeImages(dark_before_dir, 2)
+        names, keys, angles = writer.prepare_images([0, 170], proj_dir, dark_before_dir)
+        self.assertEqual(names, [*db_paths, *proj_paths])
+        self.assertEqual(keys, [2, 2, 0, 0])
+        self.assertEqual(angles, [0, 0, 0, 170])
+
+        half_circle_dir = tempfile.mkdtemp(dir=self.test_dir)
+        hc_paths = self.createFakeImages(half_circle_dir)
+        names, keys, angles = writer.prepare_images([0, 170], proj_dir, dark_before_dir, half_circle=half_circle_dir)
+        self.assertEqual(names, [*db_paths, *proj_paths, *hc_paths])
+        self.assertEqual(keys, [2, 2, 0, 0, 0])
+        self.assertEqual(angles, [0, 0, 0, 170, 180])
+        
+        flat_after_dir = tempfile.mkdtemp(dir=self.test_dir)
+        fa_paths = self.createFakeImages(flat_after_dir, 2)
+        names, keys, angles = writer.prepare_images([0, 170], proj_dir, dark_before_dir, half_circle=half_circle_dir, 
+                                                    flat_after=flat_after_dir)
+        self.assertEqual(names, [*db_paths, *proj_paths, *hc_paths, *fa_paths])
+        self.assertEqual(keys, [2, 2, 0, 0, 0, 1, 1])
+        self.assertEqual(angles, [0, 0, 0, 170, 180, 170, 170])
+
+        flat_before_dir = tempfile.mkdtemp(dir=self.test_dir)
+        fb_paths = self.createFakeImages(flat_before_dir, 3)
+        dark_after_dir = tempfile.mkdtemp(dir=self.test_dir)
+        da_paths = self.createFakeImages(dark_after_dir, 3)
+        names, keys, angles = writer.prepare_images([0, 170], proj_dir, dark_before_dir, flat_before_dir, half_circle_dir, 
+                                                    flat_after_dir, dark_after_dir)
+        self.assertEqual(names, [*db_paths, *fb_paths, *proj_paths, *hc_paths, *fa_paths, *da_paths])
+        self.assertEqual(keys, [2, 2, 1, 1, 1, 0, 0, 0, 1, 1, 2, 2, 2])
+        self.assertEqual(angles, [0, 0, 0, 0, 0, 0, 170, 180, 170, 170, 170, 170, 170])
+
+
+    def testGetTiffs(self):
+        self.assertEqual(writer.get_tiffs(self.test_dir), [])
+
+        tempfile.mkdtemp(dir=self.test_dir)
+
+        fd, _ = tempfile.mkstemp(dir=self.test_dir, suffix='.txt')
+        self.file_descriptors.append(fd)
+        self.assertEqual(writer.get_tiffs(self.test_dir), [])
+
+        paths = self.createFakeImages(self.test_dir, 2)
+        self.assertEqual(writer.get_tiffs(self.test_dir), paths)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- writer.py
+++ writer.py
@@ -1,3 +1,4 @@
+from contextlib import suppress
 import datetime
 import os
 import shutil
@@ -7,13 +8,29 @@ import tifffile
 
 
 def add_nxtomo_entry(filename, image_names, image_keys, angles):
+    """Adds nxtomo entry to the given nexus file 
+
+    :param filename: path to nexus file 
+    :type filename: str
+    :param image_names: list of images paths
+    :type image_names: List[str]    
+    :param image_keys: list of image keys 
+    :type image_keys: List[int]
+    :param angles: list of rotation angles 
+    :type angles: List[float]
+    """
+
     time = datetime.datetime.now().isoformat()
     
-    with h5py.File(filename, 'r+') as nxs_file:
+    with h5py.File(filename, 'a') as nxs_file:
         main_entry = None
         for _, item in nxs_file.items():
-            if item.attrs.get('NX_class').decode('utf-8') == 'NXentry':
+            nx_class = item.attrs.get('NX_class')
+            with suppress(AttributeError):
+                nx_class = nx_class.decode('utf-8')
+            if nx_class == 'NXentry':
                 main_entry = item
+                break
         
         if main_entry is None:
             main_entry = nxs_file.create_group('entry')
@@ -50,14 +67,11 @@ def add_nxtomo_entry(filename, image_names, image_keys, angles):
         dset[0, :, :] = image
         for index, name in enumerate(image_names[1:]):
             image = tifffile.imread(name)
-            # start = time.perf_counter() 
             dset[index + 1, :, :] = image
-            # print(time.perf_counter() - start, 'sec')
-            # print(index)
         
         sample = entry.create_group('sample')
         sample.attrs['NX_class'] = u'NXsample'
-        sample['name'] = main_entry.get('sample/title', u'fred')
+        sample['name'] =  main_entry.get('sample/name', u'')
         sample['rotation_angle'] = np.array(angles, dtype=np.float32)
         sample['rotation_angle'].attrs['units'] = 'degrees'
         sample['rotation_angle'].attrs['axis'] = 1
@@ -71,6 +85,13 @@ def add_nxtomo_entry(filename, image_names, image_keys, angles):
 
    
 def get_tiffs(image_dir):
+    """Gets list of tiff images in the given directory
+
+    :param image_dir: directory of images 
+    :type image_dir: str
+    :return: List of absolute paths of images
+    :rtype: List[str]
+    """
     images = []
 
     with os.scandir(image_dir) as it:
@@ -85,14 +106,34 @@ def get_tiffs(image_dir):
 
             images.append(entry.path)
 
-    return images
+    return sorted(images)
 
 
-def prepare_images(projections, dark_before='', flat_before='', half_circle='',  
+def prepare_images(rot_angles, projections, dark_before='', flat_before='', half_circle='',  
                    flat_after='', dark_after=''):
+    """Gets image data from given directories and place them in the appropriate order 
+
+    :param rot_angles: list of rotation angles for projection images 
+    :type rot_angles: List[float]
+    :param projections: directory of projection images 
+    :type projections: str
+    :param dark_before: directory of dark before images 
+    :type dark_before: str    
+    :param flat_before: directory of flat before images 
+    :type flat_before: str
+    :param half_circle: directory of 180 degree images 
+    :type half_circle: str
+    :param flat_after: directory of flat after images 
+    :type flat_after: str
+    :param dark_after: directory of dark after images 
+    :type dark_after: str
+    :return: List of absolute paths of images, keys and and angle
+    :rtype: Tuple[List[str], List[int], List[float]]
+    """
     if not projections:
         raise ValueError('A path to the projection images must be provided!')
     
+    start_angle, stop_angle = rot_angles[0], rot_angles[-1]
     paths = [dark_before, flat_before, projections, half_circle, flat_after, dark_after]  
     keys = [2, 1, 0, 0, 1, 2]
 
@@ -111,15 +152,45 @@ def prepare_images(projections, dark_before='', flat_before='', half_circle='',
         size = len(images)
         image_names.extend(images)
         image_keys.extend([key] * size)
-        angles.extend([0.0] * size)
+        
+        if index < 2:  # before images
+            angles.extend([start_angle] * size)
+        elif index == 2:  # projection
+            if size != len(rot_angles):
+                raise ValueError(f'The number of projection images {size} does not match the number of angles {len(rot_angles)}.')
+            angles.extend(rot_angles)
+        elif index == 3:  # 180 image
+            angles.extend([180.0] * size)
+        else:  # after images
+            angles.extend([stop_angle] * size)
 
     return image_names, image_keys, angles
 
 
-def save_tomo_to_nexus(in_filename, out_filename, projections, dark_before='', flat_before='', half_circle='',  
-                       flat_after='', dark_after=''):
-    
-    image_names, image_keys, angles = prepare_images(projections, dark_before, flat_before, half_circle,  
+def save_tomo_to_nexus(in_filename, out_filename, rot_angles, projections, dark_before='', flat_before='', 
+                       half_circle='', flat_after='', dark_after=''):
+    """Saves tomography dat to a given nexus file using the NXtomo standard
+
+    :param in_filename: path of input nexus file 
+    :type in_filename: str
+    :param out_filename: path of output nexus file 
+    :type out_filename: str
+    :param rot_angles: list of rotation angles for projection images 
+    :type rot_angles: List[float]
+    :param projections: directory of projection images 
+    :type projections: str
+    :param dark_before: directory of dark before images 
+    :type dark_before: str    
+    :param flat_before: directory of flat before images 
+    :type flat_before: str
+    :param half_circle: directory of 180 degree images 
+    :type half_circle: str
+    :param flat_after: directory of flat after images 
+    :type flat_after: str
+    :param dark_after: directory of dark after images 
+    :type dark_after: str
+    """
+    image_names, image_keys, angles = prepare_images(rot_angles, projections, dark_before, flat_before, half_circle,  
                                                      flat_after, dark_after)
     
     shutil.copyfile(in_filename, out_filename)
@@ -131,11 +202,15 @@ if __name__ == '__main__':
     out_filename = f'mod_{in_filename}'
 
     projection_path = 'D:/Downloads/dataset_phantom_rebin122_150um/modified/Tomo'
+    angles = np.linspace(0, 390, 1125).tolist()
+    
+    # Optional
     dark_before_path = 'D:/Downloads/dataset_phantom_rebin122_150um/modified/dark_before' 
     flat_before_path = 'D:/Downloads/dataset_phantom_rebin122_150um/modified/flat_before'  
     flat_after_path = 'D:/Downloads/dataset_phantom_rebin122_150um/modified/flat_after'
     dark_after_path = 'D:/Downloads/dataset_phantom_rebin122_150um/modified/dark_after'
 
-    save_tomo_to_nexus(in_filename, out_filename, projection_path, dark_before=dark_before_path,
+
+    save_tomo_to_nexus(in_filename, out_filename, angles, projection_path, dark_before=dark_before_path,
                        flat_before=flat_before_path, flat_after=flat_after_path,
                        dark_after=dark_after_path)
